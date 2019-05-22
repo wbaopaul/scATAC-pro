97d9271172d33b2f22343e16d73959553465ac26
wbaopaul <yuw1@email.chop.edu>
2019-05-22 14:13:57 -0400

    upload working folders

--- scripts/call_peak.sh
+++ scripts/call_peak.sh
@@ -8,8 +8,8 @@ mkdir -p $peaks_dir
 
 
 ## rm duplicates
-#${MACS2_PATH}/macs2 callpeak -t $input_bam --outdir $peaks_dir -f BAM $MACS2_OPTS
-${MACS2_PATH}/macs2 callpeak -t $input_bam --outdir $peaks_dir -f BAM $MACS2_OPTS --nomodel --extsize 147
+${MACS2_PATH}/macs2 callpeak -t $input_bam --outdir $peaks_dir -f BAM $MACS2_OPTS 
+#${MACS2_PATH}/macs2 callpeak -t $input_bam --outdir $peaks_dir -f BAM $MACS2_OPTS --nomodel --extsize 147
 ## filter by MAPQ
 
 
