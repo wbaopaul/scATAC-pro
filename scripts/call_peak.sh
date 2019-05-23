ca914766124cd0f47682982b028dfb07631cd7df
wbaopaul <yuw1@email.chop.edu>
2019-05-23 09:21:09 -0400

    remove peaks overlapped with blacklist

--- scripts/call_peak.sh
+++ scripts/call_peak.sh
@@ -6,11 +6,14 @@ peaks_dir="${2}/peaks"
 mkdir -p $peaks_dir
 
 
-
-## rm duplicates
-${MACS2_PATH}/macs2 callpeak -t $input_bam --outdir $peaks_dir -f BAM $MACS2_OPTS 
+## call peaks
+${MACS2_PATH}/macs2 callpeak -t $input_bam --outdir $peaks_dir -n $SAMPLE_PREFIX -f BAM $MACS2_OPTS 
 #${MACS2_PATH}/macs2 callpeak -t $input_bam --outdir $peaks_dir -f BAM $MACS2_OPTS --nomodel --extsize 147
-## filter by MAPQ
+
+
+## remove peaks overlapped with blacklist
+${BEDTOOLS_PATH}/bedtools intersect -a ${peaks_dir}/${SAMPLE_PREFIX}_peaks.narrowPeak -b $BLACKLIST -v \
+    > ${peaks_dir}/${SAMPLE_PREFIX}_peaks_filterBlacklist.narrowPeak
 
 
 
---
+++ scripts/qc_per_barcode.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+input_bam=$1
+
+## get read depth, reads of Mitocondrial, reads overlapping with annotated regions, ... per barcode 
+
+
+## get reads in peak per barcode
+
+
