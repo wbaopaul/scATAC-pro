dc647a14bb43e2bea36d1c89274d528b0ac86297
wbaopaul <yuw1@email.chop.edu>
2019-07-05 13:50:46 -0400

    edit readme

--- scripts/footprint_by_cluster.sh
+++ scripts/footprint_by_cluster.sh
@@ -23,6 +23,14 @@ if [ $USE_BIN ]; then
     exit
 fi
 
+if [ ! -f "$RGT_HINT_PATH" ]; then
+    which rgt-hint > /dev/null
+    if [ $? == 0 ]; then
+        RGT_HINT_PATH=$(dirname `which rgt-hint`)
+    fi
+    echo "RGT_HINT_PATH not provided or installed, trying to install rgt-hint:"
+fi
+
 input_peak=${OUTPUT_DIR}/peaks/${PEAK_CALLER}/${OUTPUT_PREFIX}.${MAPPING_METHOD}_peaks_BlacklistRemoved.bed  ## suppose path for peak file
 
 
@@ -31,22 +39,22 @@ unset PYTHONPATH
 echo "predict footprint..."
 cluster1=${bam1/.bam/}
 cluster1=$(basename $cluster1)
-${HINT_PATH}/rgt-hint footprinting --atac-seq --paired-end --organism=${GENOME_NAME} --output-location=$output_dir  \
+${RGT_HINT_PATH}/rgt-hint footprinting --atac-seq --paired-end --organism=${GENOME_NAME} --output-location=$output_dir  \
         --output-prefix=${cluster1} $bam1 $input_peak
 
 cluster2=${bam2/.bam/}
 cluster2=$(basename $cluster2)
-${HINT_PATH}/rgt-hint footprinting --atac-seq --paired-end --organism=${GENOME_NAME} --output-location=$output_dir  \
+${RGT_HINT_PATH}/rgt-hint footprinting --atac-seq --paired-end --organism=${GENOME_NAME} --output-location=$output_dir  \
         --output-prefix=${cluster2} $bam2 $input_peak
 
 echo "overlap with motif annotation ... "
-${HINT_PATH}/rgt-motifanalysis matching --organism=${GENOME_NAME} --input-files ${output_dir}/${cluster1}.bed ${output_dir}/${cluster2}.bed --output-location $output_dir
+${RGT_HINT_PATH}/rgt-motifanalysis matching --organism=${GENOME_NAME} --input-files ${output_dir}/${cluster1}.bed ${output_dir}/${cluster2}.bed --output-location $output_dir
 
 ncore=$(nproc --all)
 ncore=$(($ncore - 1))
 echo "number of cores: $ncore"
 echo "Differential binding analysis ..."
-${HINT_PATH}/rgt-hint differential --organism=${GENOME_NAME} --bc --nc 10 --mpbs-file1=${output_dir}/${cluster1}_mpbs.bed \
+${RGT_HINT_PATH}/rgt-hint differential --organism=${GENOME_NAME} --bc --nc 10 --mpbs-file1=${output_dir}/${cluster1}_mpbs.bed \
     --mpbs-file2=${output_dir}/${cluster1}_mpbs.bed --reads-file1=$bam1 --reads-file2=$bam2 --condition1=$cluster1 --condition2=$cluster2 --output-location=${output_dir}/${cluster1}_${cluster2}
 
 
