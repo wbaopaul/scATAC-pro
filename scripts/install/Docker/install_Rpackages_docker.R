68bf8986e9a4ef456447143bdc0818f7269471c1
wbaopaul <yuw1@email.chop.edu>
2019-11-27 10:46:50 -0500

    install additional packages

--- scripts/install/Docker/install_Rpackages_docker.R
+++ scripts/install/Docker/install_Rpackages_docker.R
@@ -14,7 +14,7 @@ for(pk in pks){
     }
 }
 
-bioc.pks = c('RColorBrewer','pheatmap','motifmatchr', 'chromVAR', 'SummarizedExperiment', 'BiocParallel', 'DESeq2', 'edgeR', 'matrixStats', 'cicero', 'farver')
+bioc.pks = c('RColorBrewer','pheatmap','motifmatchr', 'chromVAR', 'SummarizedExperiment', 'BiocParallel', 'DESeq2', 'edgeR', 'matrixStats', 'cicero', 'farver', 'org.Mm.eg.db', 'org.hs.eg.db', 'BSgenome.Hsapiens.UCSC.hg38', 'BSgenome.Hsapiens.UCSC.hg19', 'BSgenome.Mmusculus.UCSC.mm10', 'BSgenome.Mmusculus.UCSC.mm9', 'clusterProfiler')
 
 for(pk in bioc.pks){
     if(!require(pk, character.only = T)) {  
@@ -26,4 +26,9 @@ for(pk in bioc.pks){
 if(!require(chromVARmotifs)) {  
     message(paste('Install chromVARmotifs...'))
     devtools::install_github("GreenleafLab/chromVARmotifs", upgrade = 'never', force = T)
+}
+
+if(!require(cisTopic)) {  
+    message(paste('Install cisTopic...'))
+    devtools::install_github("aertslab/cisTopic", upgrade = 'never', force = T)
 }
