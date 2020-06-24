d42fbbfe1383962396c773c76588d74eed245c38
wbaopaul <yuw1@email.chop.edu>
2020-06-24 07:39:22 -0400

    start 1.1.4

--- README.md
+++ README.md
@@ -37,7 +37,7 @@ Installation
 ------------
 
 -   Note: It is not necessary to install scATAC-pro from scratch. You can use the docker or singularity version if you prefer (see [Run scATAC-pro through docker or singularity](#run-scATAC-pro-through-docker-or-singularity) )
--   Run the following command in your terminal, scATAC-pro will be installed in YOUR\_INSTALL\_PATH/scATAC-pro\_1.1.3
+-   Run the following command in your terminal, scATAC-pro will be installed in YOUR\_INSTALL\_PATH/scATAC-pro\_1.1.4
 
 <!-- -->
 
@@ -49,7 +49,7 @@ Installation
 Updates
 ------------
 - Now provide [scATAC-pro tutorial in R](https://scatacpro-in-r.netlify.app/index.html) for access QC metrics and perform downstream analysis
-- Current version: 1.1.3
+- Current version: 1.1.4
 - Recent updates
     * *runGO*: update background genes to be all genes associated with any peak
     * *integrate*: add VFACS (Variable Features Across ClusterS) option for the integration module,
@@ -269,14 +269,16 @@ Detailed Usage
     usage : scATAC-pro -s STEP -i INPUT -c CONFIG [-o] [-h] [-v]
     Use option -h|--help for more information
 
-    scATAC-pro 1.1.3
+    scATAC-pro 1.1.4
     ---------------
     OPTIONS
 
        [-s|--step ANALYSIS_STEP] : run an analysis module (or some combination of several modules) of the scATAC-pro workflow, supported modules include:
           demplx_fastq: perform demultiplexing
-                               input: fastq files for both reads and index, separated by comma like:
-                                      PE1_fastq,PE2_fastq,index1_fastq,inde2_fastq,index3_fastq...
+                               input: fastq files for both reads and index, separated by comma or folder path for 10x
+                                      fastq files like:
+                                      PE1_fastq,PE2_fastq,index1_fastq,inde2_fastq,index3_fastq...or
+                                      folder_path4_10xfastqs
                                output: Demultiplexed fastq1 and fastq2 files with index information embedded
                                        in the read name as:  @index3_index2_index1:original_read_name, saved in
                                        output/demplxed_fastq/ 
--- scripts/dex_fastq.sh
+++ scripts/dex_fastq.sh
@@ -13,68 +13,34 @@ source ${curr_dir}/read_conf.sh
 read_conf "$2"
 read_conf "$3"
 
-output_dir=${OUTPUT_DIR}/demplxed_fastq
-mkdir -p $output_dir
 
 fastqs=(${input_fastqs//,/ }) ## suppose the first two fastqs is the read file, the others are index fastq files
 nfile=${#fastqs[@]}
 kk=$(( $nfile ))
 curr_dir=`dirname $0`
 
-isSingleEnd=${isSingleEnd^^}
-if [[ "$isSingleEnd" = "TRUE" ]]; then
-    
-    if [[ $kk < 2 ]];then
-      echo -e "Erro: Provide at least two fastq files: one for read the other for index (barcode) " >&2
-      exit
-    fi
-    ## the first barcode was add to the read name after @, and : was used to concatenate to the original name
-    dex_prefix1=$(basename ${fastqs[0]})
-
-    ${PYTHON_PATH}/python ${curr_dir}/src/dex_fastq.py ${fastqs[0]} ${output_dir}/demplxed_${dex_prefix1}  ${fastqs[1]} 
-
-
-    ## for extra round of barcodes, add them to the read name after @, concatenate the original name by _
-    if [[ $kk>2 ]];then
-        for (( i==2; i<=$kk; i++ ))
-        do
-            ${PYTHON_PATH}/python ${curr_dir}/src/dex_fastq_ul.py ${output_dir}/demplxed_${dex_prefix1} ${output_dir}/demplxed_${dex_prefix1}_$i ${fastqs[$i]} 
-            mv ${output_dir}/demplxed_${dex_prefix1}_$i ${output_dir}/demplxed_${dex_prefix1}
-        done
-    fi
+## whether the input is an 10x folder
+if [[ $kk -eq 1  ]]; then
+    ## construct R1, R2 and R3 for each lane
+     R1_fastqs=($(find $1 -name '*R1*.fastq.gz' | sort ))
+     for file1 in "${R1_fastqs[@]}" 
+     do
+        file2=${file1/R1/R2}
+        file3=${file1/R1/R3}
+        bash ${curr_dir}/dex_fastq_single.sh ${file1},${file3},${file2} $2 $3 &
+     done
+     wait
+     ## cat demx fastqs from different lanes
+     find ${OUTPUT_DIR}/demplxed_fastq -name '*R1*.fastq.gz' | sort | xargs cat > ${OUTPUT_DIR}/demplxed_fastq/${OUTPUT_PREFIX}.demplxed.PE1.fastq.gz
+     find ${OUTPUT_DIR}/demplxed_fastq -name '*R3*.fastq.gz' | sort | xargs cat > ${OUTPUT_DIR}/demplxed_fastq/${OUTPUT_PREFIX}.demplxed.PE2.fastq.gz
+     rm ${OUTPUT_DIR}/demplxed_fastq/demplxed*
 else
-    if [[ $kk < 3 ]];then
-      echo -e "Erro: Provide at least three fastq files: the first two for paired-end reads the other for index (barcode) " >&2
-      exit
-    fi
-    
-    ## the first barcode was add to the read name after @, and : was used to concatenate to the original name
+    bash ${curr_dir}/dex_fastq_single.sh $1 $2 $3
     dex_prefix1=$(basename ${fastqs[0]})
     dex_prefix2=$(basename ${fastqs[1]})
-
-
-
-    ${PYTHON_PATH}/python ${curr_dir}/src/dex_fastq.py ${fastqs[0]} ${output_dir}/demplxed_${dex_prefix1}  ${fastqs[2]} &
-
-    ${PYTHON_PATH}/python ${curr_dir}/src/dex_fastq.py ${fastqs[1]} ${output_dir}/demplxed_${dex_prefix2}  ${fastqs[2]} &
-    wait
-
-    ## for the round of barcodes, add them to the read name after @, concatenate the original name by _
-    if [[ $kk>3 ]];then
-        for (( i=3; i<=$kk; i++ ))
-        do
-            ${PYTHON_PATH}/python ${curr_dir}/src/dex_fastq_ul.py ${output_dir}/demplxed_${dex_prefix1} ${output_dir}/tmp${i}_demplxed_${dex_prefix1} ${fastqs[$i]} &
-            ${PYTHON_PATH}/python ${curr_dir}/src/dex_fastq_ul.py ${output_dir}/demplxed_${dex_prefix2} ${output_dir}/tmp${i}_demplxed_${dex_prefix2} ${fastqs[$i]} &
-            wait
-            mv ${output_dir}/tmp${i}_demplxed_${dex_prefix1} ${output_dir}/demplxed_${dex_prefix1}
-            mv ${output_dir}/tmp${i}_demplxed_${dex_prefix2} ${output_dir}/demplxed_${dex_prefix2}
-        done
-    fi
-
-
-
+    mv ${OUTPUT_DIR}/demplxed_fastq/demplxed_${dex_prefix1} ${OUTPUT_DIR}/demplxed_fastq/${OUTPUT_PREFIX}.demplxed.PE1.fastq.gz
+    mv ${OUTPUT_DIR}/demplxed_fastq/demplxed_${dex_prefix2} ${OUTPUT_DIR}/demplxed_fastq/${OUTPUT_PREFIX}.demplxed.PE2.fastq.gz
 fi
-
 
 echo "Demultiplexing Done!"
 
